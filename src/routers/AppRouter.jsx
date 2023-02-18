3cbbc5bb78b951f20796e8299fdbfd7f583a801b
Belén Navas <francisbelen_nt@live.com>
2023-02-18 10:39:41 -0500

    Modified

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import CrudPages from '../pages/CrudPages';
 import HomePages from '../pages/HomePages'
 import NuestroMenu from '../pages/NuestroMenu'
+import SobreNosotros from '../pages/SobreNosotros';
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePages />} />
+                <Route path="/sobrenosotros" element={<SobreNosotros/>} />
                 <Route path="/nuestromenu" element={<NuestroMenu />} />
-                <Route path="/crud" element={<CrudPages/>} />
+                <Route path="/crud" element={<CrudPages/>} />  
             </Routes>
         </Router>
 
