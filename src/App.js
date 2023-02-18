21ae30f8f1de565c158213c464401d261299db96
Belén Navas <francisbelen_nt@live.com>
2023-02-18 01:30:22 -0500

    Error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
+import AppRouter from './routers/AppRouter'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Navbar from './components/Navbar';
-import Crud from './components/Crud';
-import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <Crud/>
-    <Footer/>
+    <AppRouter/>
     </>
   )
 }
